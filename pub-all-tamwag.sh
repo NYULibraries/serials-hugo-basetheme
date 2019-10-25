bcaa2ac7ee6d3e8cd5b394ce9f89db547226bb7a
L. Henze <lh435@nyu.edu>
2019-10-25 15:14:02 -0400

    Empty docs folder before publishing

--- pub-all-tamwag.sh
+++ pub-all-tamwag.sh
@@ -9,24 +9,30 @@ fi
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing FAWF";
+rm -rf ~/projects/fawf-nyudlts/docs/*
 hugo -e $e -s ~/projects/fawf-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Liberator";
+rm -rf ~/projects/liberator-nyudlts/docs/*
 hugo -e $e -s ~/projects/liberator-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Palante";
+rm -rf ~/projects/palante-nyudlts/docs/*
 hugo -e $e -s ~/projects/palante-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Freedom";
+rm -rf ~/projects/freedom-nyudlts/docs/*
 hugo -e $e -s ~/projects/freedom-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Southern Worker";
+rm -rf ~/projects/southernworker-nyudlts/docs/*
 hugo -e $e -s ~/projects/southernworker-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing The Masses";
+rm -rf ~/projects/themasses-nyudlts/docs/*
 hugo -e $e -s ~/projects/themasses-nyudlts/;
