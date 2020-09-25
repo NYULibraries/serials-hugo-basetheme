62b03129117c84f633634d8204d574c5b38a24ad
L. Henze <lh435@nyu.edu>
2020-09-25 13:20:07 -0400

    More succinct, adding param to empty docs directory first

--- pub-all-tamwag.sh
+++ pub-all-tamwag.sh
@@ -9,30 +9,24 @@ fi
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing FAWF";
-rm -rf ~/projects/tamwag/fawf-nyudlts/docs/*
-hugo -e $e -s ~/projects/tamwag/fawf-nyudlts/;
+hugo --cleanDestinationDir -e $e -s ~/projects/tamwag/fawf-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Liberator";
-rm -rf ~/projects/tamwag/liberator-nyudlts/docs/*
-hugo -e $e -s ~/projects/tamwag/liberator-nyudlts/;
+hugo --cleanDestinationDir -e $e -s ~/projects/tamwag/liberator-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Palante";
-rm -rf ~/projects/tamwag/palante-nyudlts/docs/*
-hugo -e $e -s ~/projects/tamwag/palante-nyudlts/;
+hugo --cleanDestinationDir -e $e -s ~/projects/tamwag/palante-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Freedom";
-rm -rf ~/projects/tamwag/freedom-nyudlts/docs/*
-hugo -e $e -s ~/projects/tamwag/freedom-nyudlts/;
+hugo --cleanDestinationDir -e $e -s ~/projects/tamwag/freedom-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Southern Worker";
-rm -rf ~/projects/tamwag/southernworker-nyudlts/docs/*
-hugo -e $e -s ~/projects/tamwag/southernworker-nyudlts/;
+hugo --cleanDestinationDir -e $e -s ~/projects/tamwag/southernworker-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing The Masses";
-rm -rf ~/projects/tamwag/themasses-nyudlts/docs/*
-hugo -e $e -s ~/projects/tamwag/themasses-nyudlts/;
+hugo --cleanDestinationDir -e $e -s ~/projects/tamwag/themasses-nyudlts/;
