b70833cd8fdb5b9f9f5a4538e2e89b479e19c6d3
L. Henze <lh435@nyu.edu>
2020-09-25 11:31:24 -0400

    Publishing script - new paths

--- pub-all-tamwag.sh
+++ pub-all-tamwag.sh
@@ -9,30 +9,30 @@ fi
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing FAWF";
-rm -rf ~/projects/fawf-nyudlts/docs/*
-hugo -e $e -s ~/projects/fawf-nyudlts/;
+rm -rf ~/projects/tamwag/fawf-nyudlts/docs/*
+hugo -e $e -s ~/projects/tamwag/fawf-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Liberator";
-rm -rf ~/projects/liberator-nyudlts/docs/*
-hugo -e $e -s ~/projects/liberator-nyudlts/;
+rm -rf ~/projects/tamwag/liberator-nyudlts/docs/*
+hugo -e $e -s ~/projects/tamwag/liberator-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Palante";
-rm -rf ~/projects/palante-nyudlts/docs/*
-hugo -e $e -s ~/projects/palante-nyudlts/;
+rm -rf ~/projects/tamwag/palante-nyudlts/docs/*
+hugo -e $e -s ~/projects/tamwag/palante-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Freedom";
-rm -rf ~/projects/freedom-nyudlts/docs/*
-hugo -e $e -s ~/projects/freedom-nyudlts/;
+rm -rf ~/projects/tamwag/freedom-nyudlts/docs/*
+hugo -e $e -s ~/projects/tamwag/freedom-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing Southern Worker";
-rm -rf ~/projects/southernworker-nyudlts/docs/*
-hugo -e $e -s ~/projects/southernworker-nyudlts/;
+rm -rf ~/projects/tamwag/southernworker-nyudlts/docs/*
+hugo -e $e -s ~/projects/tamwag/southernworker-nyudlts/;
 echo "---------------------------------";
 echo "---------------------------------\n\n";
 echo "Publishing The Masses";
-rm -rf ~/projects/themasses-nyudlts/docs/*
-hugo -e $e -s ~/projects/themasses-nyudlts/;
+rm -rf ~/projects/tamwag/themasses-nyudlts/docs/*
+hugo -e $e -s ~/projects/tamwag/themasses-nyudlts/;
