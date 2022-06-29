ce6cec1c02f685a3ae6d5484b6fa922df0607292
L. Henze <lh435@nyu.edu>
2022-06-29 13:29:25 -0400

    update JS for new viewer

--- static/js/books.js
+++ static/js/books.js
@@ -26,40 +26,30 @@ function hashHandler() {
 function reloadIframe() {
   if (window.location.hash) {
     dlts.p = window.location.hash.substring(1);
-    //console.log("dlts.p " + dlts.p);
-    //console.log("dlts.$v.dataset.sequence " + dlts.$v.dataset.sequence);
     if (dlts.p !== dlts.$v.dataset.sequence) {
-      //console.log("not same");
       dlts.$v.src = dlts.$v.src.replace(/\/([0-9]+)/, "/" + dlts.p);
       document.querySelector("body").classList.add("io-loading");
-    } 
-  }
-}
-window.addEventListener('hashchange', hashHandler, false);
-
-window.addEventListener("message", function(event) {
-  if (event.origin !== dlts.$v.dataset.baseviewerurl) return;
-  var thisData = decodeURIComponent(event.data);
-  //console.log('thisData received:  ' + thisData);
-  var vars = thisData.split("&");
-  var params = {};
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    params[pair[0]] = decodeURIComponent(pair[1]);
-  }
-  var result = JSON.parse(params.message);
-  if (result.fire) {
-    //console.log("Fire! " + result.fire);
-    if (result.fire == "button:button-fullscreen:on") {
-      hideSiblings();
-    } else if (result.fire == "display:load") {
-      document.querySelector("body").classList.remove("io-loading");
-    } else {
-      showSiblings();
     }
   }
-  if (typeof result.data !== 'undefined') {
-    //console.log("New page number " + result.data.sequence);
-    updateWrapperPage(result.data.sequence);
-  }
-}, false);
+}
+window.addEventListener("hashchange", hashHandler, false);
+
+window.addEventListener(
+  "message",
+  function (event) {
+    var data = JSON.parse(event.data);
+    if (data.fire) {
+      switch (data.fire) {
+        case "viewer:sequence:increase":
+        case "viewer:sequence:decrease":
+        case "viewer:sequence:change":
+          updateWrapperPage(data.message.sequence);
+          break;
+        case "viewer:loaded":
+          document.querySelector("body").classList.remove("io-loading");
+          break;
+      }
+    }
+  },
+  false
+);
